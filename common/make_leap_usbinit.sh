3b18caa0326fcf8aaedeeefc78a26a6ac3e0d9c4
emccann <emccann@cs.uml.edu>
2013-11-14 10:31:43 -0500

    Add smarter argument handling to make_leap_usbinit.sh

--- common/make_leap_usbinit.sh
+++ common/make_leap_usbinit.sh
@@ -1,7 +1,20 @@
 #! /bin/bash
 
 mydir=`dirname $0`
-
+if [ ! -f `dirname $0`/`basename $1` ]; then
+    $0 `dirname $0`/leap_init.pcap `dirname $0`/leap_libusb_init.c.inc $*
+    exit $?
+elif [ $# -lt 2 ]; then
+    echo "Usage: "
+    echo "  $0 <pcap file> <destination file> [ USB device addresses ... ]"
+    echo "or: "
+    echo "  $0 [ USB device addresses ... ]"
+    echo " "
+    echo "Typically, the \"USB device addresses\" contains the device number output by \"lsusb -d f182:0003\", both with leading zeros removed, and with them present."    
+    echo "If the usb device number from lsusb is 8, then you would call this script..."
+    echo "  $0 8 008"
+    exit 1
+fi 
 PCAP_FILE=$1
 OUT_FILE=$2
 shift
